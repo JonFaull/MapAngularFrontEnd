324551fb61eb1dc5b76a45215a9c3d09e5cdfa19
JonFaull <jonathan.faull@gmail.com>
2026-01-08 12:45:57 +0000

    Added temp shapefile.

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -3,6 +3,14 @@ import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import {fromLonLat} from 'ol/proj';
+import {boundingExtent} from 'ol/extent';
+
+const irelandExtent = boundingExtent([ 
+  fromLonLat([-16.185484, 55.663523]), // top-left (NW) 
+  fromLonLat([-1.112498, 51.362990]) // bottom-right (SE)
+]);
+
 
 @Component({
   selector: 'app-map',
@@ -14,24 +22,21 @@ export class MapComponent implements AfterViewInit {
   map!: Map;
 
   ngAfterViewInit(): void {
-  this.map = new Map({
-    target: 'map',
-    layers: [
-      new TileLayer({
-        source: new OSM()
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: fromLonLat([-7.6854556, 53.4217687]),
+        zoom: 7.5,
+        minZoom: 7.5,
+        maxZoom: 20,
+        constrainResolution: false,
+        extent: irelandExtent
       })
-    ],
-    view: new View({
-      center: [0, 0],
-      zoom: 2,
-      minZoom: 0,
-      maxZoom: 20,
-      constrainResolution: false
-    })
-  });
-
-  
-  //setTimeout(() => this.map.updateSize(), 50);
-}
-
+    });
+  }
 }
